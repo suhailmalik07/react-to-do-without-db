947a66d1b5e10a3a3f29e5816dc5b042f1f2fc10
suhailmalik07 <suhailmalik8057@gmail.com>
2019-07-23 00:05:27 +0530

    Updates

---
+++ precache-manifest.fa7c74230b6ad6be59ba2c5b1773acc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb105dd0288a440664ed107355ef844",
+    "url": "/react-to-do-without-db/index.html"
+  },
+  {
+    "revision": "efc991a1b712da6ca26e",
+    "url": "/react-to-do-without-db/static/css/main.818da3f5.chunk.css"
+  },
+  {
+    "revision": "999b2672c3be1c9bed28",
+    "url": "/react-to-do-without-db/static/js/2.0478774c.chunk.js"
+  },
+  {
+    "revision": "efc991a1b712da6ca26e",
+    "url": "/react-to-do-without-db/static/js/main.1257fa5c.chunk.js"
+  },
+  {
+    "revision": "f088bfa1d13f678ef1b2",
+    "url": "/react-to-do-without-db/static/js/runtime~main.4b5c9a0a.js"
+  }
+]);
